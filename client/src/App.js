7f78e807a5d6856ceeffd0ef9917d96353c882f5
Bálint <abalint88@gmail.com>
2021-09-08 20:21:52 +0200

    fix: docker, sass config, cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non reprehenderit est eos ut nesciunt nemo laborum obcaecati accusantium ipsum. Ea, commodi natus provident debitis nisi ex sapiente deserunt tenetur id!</p>
     </div>
   );
 }
